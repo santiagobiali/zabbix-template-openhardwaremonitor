b34bb8041b2660216d0cd355d068e35b764e55cb
Santiago Biali <santiago.t.biali@gmail.com>
2022-10-28 11:11:25 -0300

    Use variable instead of temporary file

--- bin/ohm_get.ps1
+++ bin/ohm_get.ps1
@@ -1,18 +1,14 @@
 # Based on the original works by captainmac40 (Zabbix Forums)
 # All changes after 1:22AM 09-03-2018 AWST should be listed via GitHub @ https://github.com/clontarfx/zabbix-template-openhardwaremonitor
-
 param([string]$ident)
-$strFileName=".\sensors.tmp"
-If (Test-Path $strFileName){
-	del $strFileName
-}
-$cle = '{"{#ID_WMI}":"'
-$valeur = '","value":"'
-$fermeture = '"}]}'
+
+$strValue=""
+
 $request = "SELECT * FROM Sensor WHERE Identifier=" + "'" + $ident + "'"
 
 Get-WmiObject -Namespace "Root\OpenHardwareMonitor" -Query $request  | ForEach-Object {
 	$clecomplete = $_.value -replace ',','.'     
-    ADD-content -path $strFileName -value $clecomplete
+    $strValue += $clecomplete
 }
-Get-content -path $strFileName
+
+echo $strValue
